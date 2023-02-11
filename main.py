2f8ca30e2977c456aa7881a94004c73e92981d41
Jeferson <cool.jef77@gmail.com>
2023-02-11 15:00:44 +0800

    x variable implementation to take strings of random that is then parsed as integer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def get_randombits(k: int):
-    numbytes = (k + 7) // 8
-    print(numbytes)
+from os import urandom
 
+def get_randombits(k: int):
+    numbytes = (k + 7) // 8 # bits / 8 and rounded up
+    x = int.from_bytes(urandom(numbytes), "big")
+    print(x)
+    
 get_randombits(100)
