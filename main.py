c3df725c037bd719c79a4dd0748f5782c8656c88
Jeferson <cool.jef77@gmail.com>
2023-02-11 15:05:40 +0800

    Implementation of gen_randomint function that generators the random integer

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ def get_randombits(k: int):
     numbytes = (k + 7) // 8 # bits / 8 and rounded up
     x = int.from_bytes(urandom(numbytes), "big")
     return x >> (numbytes * 8 - k) # trimming excess bits
-    
+
+def gen_randomint(min: int, max: int):
+    max += 1
+
+
+
 print(get_randombits(100))
