88a0a52e8dc2acf116ec4966274a5d6499f80b62
Jeferson <cool.jef77@gmail.com>
2023-02-11 15:02:59 +0800

    returning x from the get_randombits function that trims excess bits

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from os import urandom
 def get_randombits(k: int):
     numbytes = (k + 7) // 8 # bits / 8 and rounded up
     x = int.from_bytes(urandom(numbytes), "big")
-    print(x)
+    return x >> (numbytes * 8 - k) # trimming excess bits
     
-get_randombits(100)
+print(get_randombits(100))
