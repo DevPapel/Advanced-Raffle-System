9dff0aafc7901f218dc0eacf442c3045d91fe383
Jeferson <cool.jef77@gmail.com>
2023-02-15 16:21:22 +0800

    Transferred menu options conditions to become new function to be called as add_item, edit_item and spin

--- main.py
+++ main.py
@@ -37,86 +37,94 @@ def weighted_randomness(chance: list, item: list, total = 0, n = 0, k = 0):
 def main_menu():
     print("Hello",host+", what do you want to do?")
     menu = int(input("[1] Add a Raffle Prize\n[2] Edit the Raffle Prizes\n[3] Start the Raffle\n[4] End the Raffle\nEnter your answer: "))
+    return menu
 
-    if menu == 1:
-        add_item = input("What item do you want to add? ")
-        item.append(add_item)
+def add_item():
+    add_item = input("What item do you want to add? ")
+    item.append(add_item)
 
-        add_chance = float(input("What is the percentage chance to get "+str(add_item)+"? "))
-        chance.append(add_chance)
+    add_chance = float(input("What is the percentage chance to get "+str(add_item)+"? "))
+    chance.append(add_chance)
 
-        print(item)
-        print(chance)
+    print(item)
+    print(chance)
 
+def edit_item():
+    print("\n- Editing [Type the number you want to edit] -")
+    print("[0] Go back to Main Menu")
+    for i in range(len(item)):
+        print("["+str(i+1)+"]", item[i], str(chance[i])+"%")
+    edit_menu1 = int(input("What item do you want to edit? "))
+    if edit_menu1 != 0:
+        edit_menu1 = edit_menu1 - 1
+    else:
+        print("")
+        main_menu()
+    print("\n- Editing "+str(item[edit_menu1]),str(chance[edit_menu1])+"% [Choose an option] -")
+    print("[0] Go Back to Main Menu\n[1] Change Item & Chance\n[2] Change the Item\n[3] Change the Chance\n[4] Remove the item from the raffle")
 
-    elif menu == 2:
-        print("\n- Editing [Type the number you want to edit] -")
-        print("[0] Go back to Main Menu")
-        for i in range(len(item)):
-            print("["+str(i+1)+"]", item[i], str(chance[i])+"%")
-        edit_menu1 = int(input("What item do you want to edit? "))
-        if edit_menu1 != 0:
-            edit_menu1 = edit_menu1 - 1
-        else:
-            print("")
-            main_menu()
-        print("\n- Editing "+str(item[edit_menu1]),str(chance[edit_menu1])+"% [Choose an option] -")
-        print("[0] Go Back to Main Menu\n[1] Change Item & Chance\n[2] Change the Item\n[3] Change the Chance\n[4] Remove the item from the raffle")
-
-        edit_menu2 = int(input("What do you want to edit from "+str(item[edit_menu1])+" "+str(chance[edit_menu1])+"%? "))
-        if edit_menu2 == 0:
-            print("")
-            main_menu()
-        elif edit_menu2 == 1:
-            new_item = input("Type the new item you want to replace "+str(item[edit_menu1]+": "))
-            item[edit_menu1] = new_item
-            new_chance = float(input("Type the new chance for the item "+str(item[edit_menu1])+": "))
-            chance[edit_menu1] = new_chance
-            print("Raffle Item have successfully been updated")
-            print("\nPreviewing Raffle items to win")
-            for i in range(len(item)):
-                print("-", item[i], str(chance[i])+"%")
-
-        elif edit_menu2 == 2:
-            new_item = input("Type the new item you want to replace "+str(item[edit_menu1]+": "))
-            item[edit_menu1] = new_item
-            print("Raffle Item have successfully been updated")
-            print("\nPreviewing Raffle items to win")
-            for i in range(len(item)):
-                print("-", item[i], str(chance[i])+"%")
-
-        elif edit_menu2 == 3:
-            new_chance = float(input("Type the new chance for the item "+str(item[edit_menu1])+": "))
-            chance[edit_menu1] = new_chance
-            print("Raffle Item have successfully been updated")
-            print("\nPreviewing Raffle items to win")
-            for i in range(len(item)):
-                print("-", item[i], str(chance[i])+"%")
-
-        elif edit_menu2 == 4:
-            del item[edit_menu1]
-            del chance[edit_menu1]
-            print("Raffle Item have successfully been updated")
-            print("\nPreviewing Raffle items to win")
-            for i in range(len(item)):
-                print("-", item[i], str(chance[i])+"%")
-
-    elif menu == 3:
-        #Spin 1x Spin X Times Spin Again Main Menu
+    edit_menu2 = int(input("What do you want to edit from "+str(item[edit_menu1])+" "+str(chance[edit_menu1])+"%? "))
+    if edit_menu2 == 0:
+        print("")
+        main_menu()
+    elif edit_menu2 == 1:
+        new_item = input("Type the new item you want to replace "+str(item[edit_menu1]+": "))
+        item[edit_menu1] = new_item
+        new_chance = float(input("Type the new chance for the item "+str(item[edit_menu1])+": "))
+        chance[edit_menu1] = new_chance
+        print("Raffle Item have successfully been updated")
         print("\nPreviewing Raffle items to win")
         for i in range(len(item)):
             print("-", item[i], str(chance[i])+"%")
-        spin = int(input("\n- Choose an option -\n[1] Spin 1x\n[2] Spin X Times \n[3] Go back to Main Menu\nType the option you want: "))
-        if spin == 0:
-            print("")
-            main_menu()
-        elif spin == 1:
-            winning_prize = weighted_randomness(chance, item)
-            print("Congratulations, you got the raffle item "+winning_prize+"!")
-        elif spin == 2:
-            spin_option = int(input("How many spin you want to do?[Example answer: 10 - will be 10x Spin]: "))
-            for i in range(0, spin_option):
-                print("Congraulations, you got the raffle item "+weighted_randomness(chance,item)+"!")
+
+    elif edit_menu2 == 2:
+        new_item = input("Type the new item you want to replace "+str(item[edit_menu1]+": "))
+        item[edit_menu1] = new_item
+        print("Raffle Item have successfully been updated")
+        print("\nPreviewing Raffle items to win")
+        for i in range(len(item)):
+            print("-", item[i], str(chance[i])+"%")
+
+    elif edit_menu2 == 3:
+        new_chance = float(input("Type the new chance for the item "+str(item[edit_menu1])+": "))
+        chance[edit_menu1] = new_chance
+        print("Raffle Item have successfully been updated")
+        print("\nPreviewing Raffle items to win")
+        for i in range(len(item)):
+            print("-", item[i], str(chance[i])+"%")
+
+    elif edit_menu2 == 4:
+        del item[edit_menu1]
+        del chance[edit_menu1]
+        print("Raffle Item have successfully been updated")
+        print("\nPreviewing Raffle items to win")
+        for i in range(len(item)):
+            print("-", item[i], str(chance[i])+"%")
+
+def spin():
+    print("\nPreviewing Raffle items to win")
+    for i in range(len(item)):
+        print("-", item[i], str(chance[i])+"%")
+    spin = int(input("\n- Choose an option -\n[1] Spin 1x\n[2] Spin X Times \n[3] Go back to Main Menu\nType the option you want: "))
+    if spin == 0:
+        print("")
+        main_menu()
+    elif spin == 1:
+        winning_prize = weighted_randomness(chance, item)
+        print("Congratulations, you got the raffle item "+winning_prize+"!")
+    elif spin == 2:
+        spin_option = int(input("How many spin you want to do?[Example answer: 10 - will be 10x Spin]: "))
+        for i in range(0, spin_option):
+            print("Congraulations, you got the raffle item "+weighted_randomness(chance,item)+"!")
+
+def main_menu2(menu):
+    if menu == 1:
+        add_item()
+    elif menu == 2:
+        edit_item()
+    elif menu == 3:
+        spin()
+        
 
 item = ["stone", "sword"]
 chance = [1, 2]
@@ -124,7 +132,8 @@ chance = [1, 2]
 print("Welcome to the Advanced Raffle System")
 host = input("Please enter the Host's Name: ")
 
-main_menu()
+option = main_menu()
+main_menu2(option)
             
 
     
